e231bc57fce2e838927070c4cf55bfceaf3741a3
paragdengale <paragdengale2017@gmail.com>
2021-12-07 13:46:44 +0530

    git

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import data from './data'
+import Product from './components/Product';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+        <div className="grid-container">
+
+            <header className="row">
+                <div>
+                    <a className="brand" href="/">Grocery Shop</a>
+                </div>
+                <div>
+                    <a href="/cart">Cart</a>
+                    <a href="/signin">Sign In </a>
+                </div>
+
+
+            </header>
+            <main>
+                <div className="row center">
+                    {data.products.map((product =>
+                        <Product key={product._id} product={product}> </Product>
+                    ))
+
+                    }
+
+                </div>
+
+
+            </main>
+            <footer className="row center">
+
+                All right reserved
+</footer>
+
+        </div>
+    );
 }
 
 export default App;
